7fc1f54a76179292c0ec726ba77bea4b7334711b
ariel_fenster <arielfenster1610@gmail.com>
2020-08-29 23:26:09 +0300

    Updates

---
+++ precache-manifest.8bc75c7392ef4809b12f40f775f41c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d45d83e11f8f24d1ee0797ad701674c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e87c658874ea8053fab7",
+    "url": "/static/css/main.da320a25.chunk.css"
+  },
+  {
+    "revision": "fd507b5afc04d8333e24",
+    "url": "/static/js/2.2bb22967.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2bb22967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87c658874ea8053fab7",
+    "url": "/static/js/main.1172e5b8.chunk.js"
+  },
+  {
+    "revision": "5a7f5984a750e59fe3b7",
+    "url": "/static/js/runtime-main.2ba6b2f4.js"
+  }
+]);
